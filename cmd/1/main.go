f7bbf9861e6e13f5b08b0094c2f8ae0940d13bb8
Markus Hubig <mhubig@hey.com>
2023-12-04 00:31:41 +0100

    Refactor for better testability

--- cmd/1/main.go
+++ cmd/1/main.go
@@ -14,16 +14,23 @@ import (
 var data string
 
 func main() {
-	result := computeCalibrationValueSum(data)
-	fmt.Println(result)
+	regexPattern1 := "[1-9]"
+	regexPattern2 := "[0-9]|one|two|three|four|five|six|seven|eight|nine"
+
+	result1 := computeCalibrationValueSum(data, regexPattern1)
+	fmt.Println("Part 1:", result1)
+
+	result2 := computeCalibrationValueSum(data, regexPattern2)
+	fmt.Println("Part 2:", result2)
 }
 
-func computeCalibrationValueSum(data string) int {
+func computeCalibrationValueSum(data string, pattern string) int {
+	regex := regexp.MustCompile(pattern)
 	lines := strings.Split(data, "\n")
 
 	var numbers []int
 	for i := 0; i < len(lines); i++ {
-		numbers = append(numbers, findCalibrationValue(lines[i]))
+		numbers = append(numbers, findCalibrationValue(lines[i], regex))
 	}
 
 	var result int
@@ -34,17 +41,65 @@ func computeCalibrationValueSum(data string) int {
 	return result
 }
 
-func findCalibrationValue(text string) int {
-	re := regexp.MustCompile("[0-9]")
-	digits := re.FindAllString(text, -1)
+func findCalibrationValue(line string, regex *regexp.Regexp) int {
+	var digits []string
+	for i := 0; i < len(line); i++ {
+		found := regex.FindString(line[i:])
+		if found == "" {
+			continue
+		}
+		digits = append(digits, found)
+	}
 
-	first := digits[0]
-	last := digits[len(digits)-1]
+	sanitizedDigits := sanitizeDigits(digits)
+
+	first := sanitizedDigits[0]
+	last := sanitizedDigits[len(sanitizedDigits)-1]
 
 	calibrationValue, err := strconv.Atoi(fmt.Sprintf("%s%s", first, last))
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	//fmt.Println(line, digits, sanitizedDigits, calibrationValue)
 	return calibrationValue
+}
+
+func sanitizeDigits(digits []string) []string {
+	var sanitizedDigits []string
+
+	for i := 0; i < len(digits); i++ {
+		if len(digits[i]) > 1 {
+			sanitizedDigits = append(sanitizedDigits, convertToNumber(digits[i]))
+		} else {
+			sanitizedDigits = append(sanitizedDigits, digits[i])
+		}
+	}
+
+	return sanitizedDigits
+}
+
+func convertToNumber(number string) string {
+	switch number {
+	case "one":
+		return "1"
+	case "two":
+		return "2"
+	case "three":
+		return "3"
+	case "four":
+		return "4"
+	case "five":
+		return "5"
+	case "six":
+		return "6"
+	case "seven":
+		return "7"
+	case "eight":
+		return "8"
+	case "nine":
+		return "9"
+	default:
+		return "0"
+	}
 }
--- cmd/1/main_test.go
+++ cmd/1/main_test.go
@@ -5,12 +5,27 @@ import (
 	"testing"
 )
 
-//go:embed data-test.txt
-var testData string
+//go:embed data-test-1.txt
+var testData1 string
 
-func TestFindCalibrationValue(t *testing.T) {
+//go:embed data-test-2.txt
+var testData2 string
+
+func TestFindCalibrationValue_WithTestData_1(t *testing.T) {
 	expected := 142
-	result := computeCalibrationValueSum(testData)
+	regexPattern := "[1-9]"
+	result := computeCalibrationValueSum(testData1, regexPattern)
+
+	if result != expected {
+		t.Errorf("Expected %d, got %d", expected, result)
+	}
+}
+
+func TestFindCalibrationValue_WithTestData_2(t *testing.T) {
+	expected := 281
+	regexPattern := "[0-9]|one|two|three|four|five|six|seven|eight|nine"
+	result := computeCalibrationValueSum(testData2, regexPattern)
+
 	if result != expected {
 		t.Errorf("Expected %d, got %d", expected, result)
 	}
